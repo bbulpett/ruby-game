65c33dc9738abe32d69465883b19057a41e113dc
Barnabas Bulpett <barnabasbulpett@gmail.com>
2016-11-20 20:16:18 -0500

    Tileize spaceship sheet and draw initial position

--- main.rb
+++ main.rb
@@ -5,13 +5,14 @@ class SpaceGameWindow < Gosu::Window
     super(960, 480)
     self.caption = "Space Game"
 
-    @spaceship = Gosu::Image.new("assets/spaceship.png")
     @background = Gosu::Image.new("assets/background.png")
+    @spaceship = Gosu::Image.load_tiles("assets/green_spaceships_sheet.png", 116, 50)
+    @current_spaceship_image = @spaceship[1]
   end
 
   def draw
     @background.draw(0, 0, 0)
-    @spaceship.draw(50, 348, 1)
+    @current_spaceship_image.draw(50, 240, 1)
   end
 end
 
